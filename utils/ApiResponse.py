e6ec495a65d10bceb0160d501e9482ac5f23aae9
Jinx <1012140020@qq.com>
2022-04-29 23:11:47 +0800

    Init commit

--- utils/ApiResponse.py
+++ utils/ApiResponse.py
@@ -1,9 +1,26 @@
+import json
+
+from django.core import serializers
+
+
 class ApiResponse:
-    def api_reponse(data) -> object:
-        result = {
-            'code': 0,
-            'msg': '获取成功',
-            'data': data
-        }
+    def api_reponse(msg) -> object:
+        result = {"code": 0, "msg": "", "data": None}
         return result
 
+    def api_reponse(data) -> object:
+        try:
+            result = []
+            data_dict = json.loads(serializers.serialize('json', data))
+            print(data)
+            for temp in data_dict:
+                print(temp["pk"])
+                result.append(temp["fields"])
+            for temp in data_dict:
+                for t in result:
+                    t["id"] = temp["pk"]
+            print(result)
+        except Exception as e:
+            print(e)
+        result = {"code": 0, "msg": "", "data": result}
+        return result
