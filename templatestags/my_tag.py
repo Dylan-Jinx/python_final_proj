1f44592d11e208de306e97a848c98ef19c9ed8c2
Jinx <1012140020@qq.com>
2022-05-10 21:08:23 +0800

    templates file and configuration completed

--- templatestags/my_tag.py
+++ templatestags/my_tag.py
@@ -14,6 +14,15 @@ def area_convert(value, arg):
 
 
 @register.filter()
+def real_area_detail(value, arg):
+    areaInfo = Area.objects.filter(code=arg).first()
+    cityInfo = City.objects.filter(code=areaInfo.citycode).first()
+    provinceInfo = Province.objects.filter(code=cityInfo.provincecode).first()
+    value = f'{provinceInfo.name}{cityInfo.name}{areaInfo.name}'
+    return value
+
+
+@register.filter()
 def datetime_convert(value, arg):
     value = dt.datetime.strftime(arg, '%Y-%m-%d')
     return value
